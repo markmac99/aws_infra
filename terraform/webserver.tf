5a16eae54498bf95121c8da6677149d48f330113
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2025-03-11 16:38:27 +0000

    add SG rule for 3306 on the webserver

--- terraform/webserver.tf
+++ terraform/webserver.tf
@@ -94,3 +94,11 @@ resource "aws_vpc_security_group_ingress_rule" "rule_ssh" {
   ip_protocol       = "tcp"
   to_port           = 22
 }
+
+resource "aws_vpc_security_group_ingress_rule" "rule_mysql" {
+  security_group_id = aws_security_group.webserver_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 3306
+  ip_protocol       = "tcp"
+  to_port           = 3306
+}
