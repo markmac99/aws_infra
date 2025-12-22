5bfb5c863942b0838c5d83baea27e102c189664a
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2025-12-22 20:03:29 +0000

    add missin sg rule

--- terraform/batchserver.tf
+++ terraform/batchserver.tf
@@ -134,7 +134,18 @@ resource "aws_security_group" "ec2publicsg" {
       security_groups  = []
       self             = false
       to_port          = -1
-    }    
+    }, 
+          {
+              cidr_blocks      = ["0.0.0.0/0"]
+              description = "mariadb"
+              from_port        = 3306
+              ipv6_cidr_blocks = []
+              prefix_list_ids  = []
+              protocol         = "tcp"
+              security_groups  = []
+              self             = false
+              to_port          = 3306
+            },    
 
   ]
   egress = [
