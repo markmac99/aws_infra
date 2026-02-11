895843fec71ef3e1aca4ddca0021f6a993b9e420
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2026-02-11 21:57:26 +0000

    use correct profile when imaging the test server

--- scripts/reimageTestServer.ps1
+++ scripts/reimageTestServer.ps1
@@ -1,3 +1,3 @@
 $instdetails=(aws ec2 describe-instances --filters Name="tag:Name",Values="testserver" --profile default)
 $instanceid= (Write-Output $instdetails| convertfrom-json)[0].reservations.instances.instanceid
-aws ec2 create-image --instance-id $instanceid --name "WMPL Testing" --description "Latest WMPL testing image"
+aws ec2 create-image --instance-id $instanceid --name "WMPL Testing" --description "Latest WMPL testing image" --profile default
